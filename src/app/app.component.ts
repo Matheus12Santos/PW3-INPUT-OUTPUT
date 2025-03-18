22ff159c780516ca692b5db6dfaf0d608587e8c8
Matheus12Santos <matheus.vieira137@etec.sp.gov.br>
2025-03-18 11:00:04 -0300

    Introducao ao Output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TextOverflowComponent } from "./text-overflow/text-overflow.component";
 import { BiscoitoComponent } from "./biscoito/biscoito.component";
+import { AleatorioComponent } from "./aleatorio/aleatorio.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TextOverflowComponent, BiscoitoComponent],
+  imports: [RouterOutlet, TextOverflowComponent, BiscoitoComponent, AleatorioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PW3-INPUT-OUTPUT';
+
+  resultado: string = "";
+
+  processar(valor:string) {
+    this.resultado = valor;
+  }
 }
